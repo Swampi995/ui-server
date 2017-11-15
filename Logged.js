3bc721c68fe8f21a18e6e67c1fb07d950b3d9431
Swampi995 <swp.mrs@gmail.com>
2017-11-15 14:57:14 +0200

    Add files via upload

---
+++ Logged.js
@@ -0,0 +1,22 @@
+import {IconButton, IconMenu, MenuItem} from 'material-ui';
+import * as React from 'react';
+import PropTypes from 'prop-types'
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
+
+export const Logged = (props) => (
+  <IconMenu
+    iconButtonElement={
+      <IconButton><MoreVertIcon/></IconButton>
+    }
+    targetOrigin={{horizontal: 'right', vertical: 'top'}}
+    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+  >
+    <MenuItem primaryText='Refresh'/>
+    <MenuItem primaryText='Help'/>
+    <MenuItem onClick={props.logged} primaryText='Sign out'/>
+  </IconMenu>
+)
+
+Logged.propTypes = {
+  logged: PropTypes.func,
+}
